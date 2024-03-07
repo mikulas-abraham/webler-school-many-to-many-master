0904bca7abed645d3f95af640f5f38b643c90131
Ábrahám Miklós <mikcsek2@gmail.com>
2024-03-07 17:26:56 +0100

    Initial commit

---
+++ src/main/java/hu/webler/weblerschoolmanytomany/WeblerSchoolManyToManyApplication.java
@@ -0,0 +1,13 @@
+package hu.webler.weblerschoolmanytomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblerSchoolManyToManyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeblerSchoolManyToManyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/webler/weblerschoolmanytomany/WeblerSchoolManyToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.webler.weblerschoolmanytomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblerSchoolManyToManyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
